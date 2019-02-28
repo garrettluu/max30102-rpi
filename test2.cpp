5455a3d632649280ce073065f008aebb0b4a975d
Garrett Luu <garrettluu131@gmail.com>
2019-02-28 00:42:19 -0800

    Implemented untested ws client

--- test2.cpp
+++ test2.cpp
@@ -12,12 +12,16 @@ int main() {
 	HeartRate heartRate(sensor);
 	heartRate.begin();
 	using easywsclient::WebSocket;
+  //Server on port 3000
 	Websocket::pointer ws = WebSocket::from_url("ws://localhost:3000");
 	assert(ws);
 	cout << "Began heart rate calculation..." << endl;
 	while (1) {
 		cout << "IR Heart Rate- Latest:" << heartRate.getLatestIRHeartRate() << ", SAFE:" << heartRate.getSafeIRHeartRate();
-		cout << ", Temperature: " << heartRate.getLatestTemperatureF() << endl;
+		//cout << ", Temperature: " << heartRate.getLatestTemperatureF() << endl;
+    ws->poll();
+    //Send the heart rate data to the server
+    ws->send(heartRate.getSafeIRHeartRate());
 		usleep(1000000);
 	}
 	return 0;
